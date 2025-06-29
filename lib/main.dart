f0f2079a82fffa012631472e38cd53dd22d97969
musabEltijaniAhmed <thkel.m.app@almusanid.com>
2025-06-29 16:21:14 +0300

    Remove Firebase configuration and clean up project files; deleted firebase_options.dart and custom_app_bar_title.dart, and updated main.dart to eliminate unused imports and initialization code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
 import 'dart:async';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_project_template/core/error/error_log.dart';
 import 'package:flutter_project_template/core/shared/class_shared_import.dart';
 
 import 'app/app.dart';
-import 'core/services/notifications/initialize_notification.dart';
-import 'firebase_options.dart';
 
 void main() async {
   runZonedGuarded(
@@ -18,18 +13,7 @@ void main() async {
       WidgetsFlutterBinding.ensureInitialized();
       await EasyLocalization.ensureInitialized();
       WidgetsFlutterBinding.ensureInitialized();
-
       await ScreenUtil.ensureScreenSize();
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-
-      ///Initialize services
-      await InitializeNotification.initialize();
-      FirebaseMessaging.onBackgroundMessage(
-        InitializeNotification.onBackgroundMessage,
-      );
-
       /// Allow only portrait mode
       SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
