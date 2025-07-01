c1e0da83c6f29a29bb8484b427b81e15817dc312
musabEltijaniAhmed <thkel.m.app@almusanid.com>
2025-07-01 09:57:24 +0300

    Update project configuration and paths to reflect new user environment; modified .flutter-plugins, pubspec.yaml, and package configuration files to replace user-specific paths, and adjusted AndroidManifest.xml and build.gradle.kts for project naming conventions.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,8 @@ plugins {
 }
 
 android {
-    namespace = "{{package_name}}"
+    namespace = name: {{project_name_snake}}
+description: {{description}}
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -25,7 +26,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.almusanid.flutter_projects_template"
+        applicationId = name: {{project_name_snake}}
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- lib/features/projects/presentation/widgets/header.dart
+++ lib/features/projects/presentation/widgets/header.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project_template/core/shared/class_shared_import.dart';
 import 'package:flutter_project_template/core/widgets/app_badges.dart';
-import 'package:flutter_project_template/core/util/print_info.dart';
 
 class Header extends StatelessWidget {
   const Header({super.key});
