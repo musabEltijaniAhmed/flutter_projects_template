c367a26a2239c569ebf93b6bbef690ca5f1f8ee4
musabEltijaniAhmed <thkel.m.app@almusanid.com>
2025-07-01 10:10:15 +0300

    Update Android build configuration to use package name for namespace and application ID

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,8 +9,7 @@ plugins {
 }
 
 android {
-    namespace = name: {{project_name_snake}}
-description: {{description}}
+    namespace = "{{package_name}}"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -26,7 +25,7 @@ description: {{description}}
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = name: {{project_name_snake}}
+        applicationId = "{{package_name}}"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
